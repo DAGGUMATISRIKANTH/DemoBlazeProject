9d03179fe8ba093fb83d32a1372448df5041ab0d
AnjaliChintham <60094067+AnjaliChintham@users.noreply.github.com>
2020-03-26 10:09:29 +0530

    Update Login_testRunner.java

--- src/test/java/Com/testrunner/Login_testRunner.java
+++ src/test/java/Com/testrunner/Login_testRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/main/resources/Feature/Testcase.feature",
 plugin = {"pretty","html:reports/cucumber-html-report"},
-tags= {"@tc01_demoblazelogin,@tc02_demoblazeAddtoCart,@tc03_ContactDetails,
+tags= {"@tc01_demoblazelogin,@tc02_demoblazeAddtoCart,@tc03_ContactDetails,@tc04_nextpage"},
 glue= {"steps"},
 monochrome=true
 )
