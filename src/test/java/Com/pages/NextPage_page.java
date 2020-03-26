c317d954b493bf21c8950d31160173142e2292ee
DAGGUMATISRIKANTH <60091725+DAGGUMATISRIKANTH@users.noreply.github.com>
2020-03-26 13:29:11 +0530

    Update NextPage_page.java

--- src/test/java/Com/pages/NextPage_page.java
+++ src/test/java/Com/pages/NextPage_page.java
@@ -16,18 +16,34 @@ import org.openqa.selenium.interactions.Actions;
 
 public class NextPage_page {
 	WebDriver driver;
-	WebElement textbox;
+		
+		public void url(String browser) 
+		{
+			//To launch Chrome Browser
+			if(browser.equalsIgnoreCase("Chrome"))
+			{
+		System.setProperty("webdriver.chrome.driver", "D:\\chrome driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+			}
+			//To launch Firefox Driver
+			else if(browser.equalsIgnoreCase("Firefox"))
+			{
+				System.setProperty("webdriver.firefox.driver", "D:\\java app\\geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+			//To launch InternetExplorerDriver
+			else if(browser.equalsIgnoreCase("IEDriver"))
+			{
+				System.setProperty("webdriver.ie.driver", "D:\\java app\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+			}
+			//To Maximize the Window
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
 
-	public void url() 
-	{
-	System.setProperty("webdriver.chrome.driver", "D:\\Selenium Jars\\Chrome jars\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	driver.get("https://google.com");
-	System.out.println(driver.getTitle());
-
-	}
+		}
 	public void demoblaze_homePage()
 	{
 		driver.get("https://demoblaze.com/index.html");
