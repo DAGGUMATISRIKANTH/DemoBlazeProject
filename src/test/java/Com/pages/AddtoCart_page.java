97bc361a9545be207be6375b7e4ecb0bf8ff6b33
DAGGUMATISRIKANTH <60091725+DAGGUMATISRIKANTH@users.noreply.github.com>
2020-03-26 13:25:58 +0530

    Update AddtoCart_page.java

--- src/test/java/Com/pages/AddtoCart_page.java
+++ src/test/java/Com/pages/AddtoCart_page.java
@@ -14,18 +14,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddtoCart_page {
 	WebDriver driver;
-	WebElement textbox;
+		
+		public void url(String browser) 
+		{
+			//To launch Chrome Browser
+			if(browser.equalsIgnoreCase("Chrome"))
+			{
+		System.setProperty("webdriver.chrome.driver", "D:\\chrome driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+			}
+			//To launch Firefox Driver
+			else if(browser.equalsIgnoreCase("Firefox"))
+			{
+				System.setProperty("webdriver.firefox.driver", "D:\\java app\\geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+			//To launch InternetExplorerDriver
+			else if(browser.equalsIgnoreCase("IEDriver"))
+			{
+				System.setProperty("webdriver.ie.driver", "D:\\java app\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+			}
+			//To Maximize the Window
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
 
-	public void url() //Launching Chrome browser
-	{
-	System.setProperty("webdriver.chrome.driver", "D:\\Selenium Jars\\Chrome jars\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	driver.get("https://google.com");
-	System.out.println(driver.getTitle());
-
-	}
+		}
 	public void demoblaze_homePage()//Opening Demoblaze Application
 	{
 		driver.get("https://demoblaze.com/index.html");
@@ -42,7 +58,14 @@ public class AddtoCart_page {
 	{
 		WebElement b=driver.findElement(By.xpath("//*[@id=\"tbodyid\"]//div[2]//div//a"));
 		b.click();
+		Screenshot("C:\\Users\\triveni\\Desktop\\Projectframework\\src\\test\\resources\\Screenshot\\AddtoCart1.jpg");
 		
+	}
+	public void Screenshot(String path) throws IOException    //  To take screenshot
+	{
+		TakesScreenshot ts=((TakesScreenshot)driver);
+		File source=ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(source, new File(path));
 	}
 
 }
